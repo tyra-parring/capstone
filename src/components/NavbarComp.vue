40d31cd05b460add3ac16b3587eadc62508ec63b
Tyra <parringt@gmail.com>
2024-08-28 11:46:33 +0200

    added templates in all views; added routes in index.js; worked on navbar

---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,91 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Jewelry Ecommerce</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Products</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Contact Us</a>
+            </li>
+            <li class="nav-item" v-if="isAdmin">
+              <a class="nav-link" href="#">Admin</a>
+            </li>
+          </ul>
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="#" v-if="!isLoggedIn">
+                <i class="fas fa-user"></i> Login / Sign Up
+              </a>
+              <a class="nav-link" href="#" v-if="isLoggedIn">
+                <i class="fas fa-user"></i> Profile
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">
+                <i class="fas fa-shopping-cart"></i> Cart
+              </a>
+            </li>
+          </ul>
+          <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+    </nav>
+  </template>
+  
+  <script>
+  export default {
+    name: 'NavbarComp',
+    data() {
+      return {
+        isLoggedIn: false,
+        isAdmin: false
+      }
+    }
+  }
+  </script>
+  
+  <style scoped>
+  .navbar {
+    padding: 1rem 2rem;
+  }
+  
+  .navbar-brand {
+    font-weight: bold;
+    color: #333;
+  }
+  
+  .nav-link {
+    color: #666;
+    transition: color 0.2s ease;
+  }
+  
+  .nav-link:hover {
+    color: #333;
+  }
+  
+  .navbar-toggler-icon {
+    font-size: 1.5rem;
+  }
+  
+  @media (max-width: 991px) {
+    .navbar-collapse {
+      background-color: #f9f9f9;
+      padding: 1rem;
+    }
+  }
+  </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ProductsView from '@/views/ProductsView.vue'
+import AdminView from '@/views/AdminView.vue'
+import ContactView from '@/views/ContactView.vue'
+import ProductView from '@/views/ProductView.vue'
+
 
 const routes = [
   {
@@ -14,6 +19,36 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/products',
+    name: 'products',
+    component: ProductsView
+  },
+  {
+    path: '/product',
+    name: 'product',
+    component: ProductView
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    component: AdminView
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: ContactView
+  },
+  {
+    path: '/product/:id',
+    name: 'product',
+    component: ProductView,
+  },
+  {
+    path: '/admin/products',
+    name: 'AdminView',
+    component: AdminView
   }
 ]
 
