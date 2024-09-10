a997672a182168a34e9c4ff41db21629e5f851b1
Tyra Parring <parringt@gmail.com>
2024-09-10 09:47:10 +0200

    modified index.js due to cors issue

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ let port = process.env.PORT || 1003
 const app = express()
 app.use(express.json())
 app.use(cors({
-    origin: '*',
-    Credentials:true
+    origin: 'http://localhost:8080',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
 }))
 
 app.use(express.static('public'))
