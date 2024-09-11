25597b7048de6ae989de1ba74ce3c420fd8d003e
Tyra Parring <parringt@gmail.com>
2024-09-11 13:36:34 +0200

    imported toastify to main.js

--- src/main.js
+++ src/main.js
@@ -12,7 +12,16 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { faUser, faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css'
 
 library.add(faUser, faShoppingCart)
-createApp(App).use(store).use(router).component('NavbarComp', NavbarComp)
-.component('FooterComp', FooterComp).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
+
+createApp(App).use(store)
+.use(router).component('NavbarComp', NavbarComp)
+.component('FooterComp', FooterComp)
+.component('font-awesome-icon', FontAwesomeIcon)
+.use(Toast)
+.mount('#app')
+
+
