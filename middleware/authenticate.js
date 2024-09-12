0394944bf077f238c060433878f350c915cf94a7
Tyra Parring <parringt@gmail.com>
2024-09-12 12:16:35 +0200

    worked on authentication functions

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,59 +1,53 @@
 import { compare } from "bcrypt";
 import { getUserDb } from "../model/usersDb.js";
-import jwt from 'jsonwebtoken'
-import {config} from 'dotenv';
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
 
-config()
+config();
 
-const checkUser = async (req, res, next) => {
-    const { emailAdd, userPass } = req.body;
-    let user = await getUserDb(emailAdd)
-    let hashedPassword = user.userPass
-    // console.log(hashedPassword)
-    let result = await compare(userPass, hashedPassword);
-    // console.log(result);
-    try {
-        if (result == true) {
-            let token = jwt.sign({ emailAdd: emailAdd }, process.env.SECRET_KEY, { expiresIn: '1hr' });
-            console.log(token);
-            jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-                console.log(token);
-                if (err) {
-                    res.json({ message: 'Token is invalid' });
-                } else {
-                    req.body.emailAdd = decoded.emailAdd;
-                    // res.json({ message: 'Token is valid' });
-                }
-            });
-            req.body.token = token;
-            console.log(token)
-            next();
-            return;
-        } else {
-            res.send('Password is incorrect');
-        }
-    } catch (e) {
-        res.json({
-            status: 404,
-            message: ('An Error has occured')
-        });
-    }
+const authenticateUser = async (req, res, next) => {
+  const { emailAdd, userPass } = req.body;
+  const user = await getUserDb(emailAdd);
+
+  if (!user) {
+    res.status(401).json({ message: "User not found" });
+    return;
+  }
+
+  const hashedPassword = user.userPass;
+  const isValidPassword = await compare(userPass, hashedPassword);
+
+  if (!isValidPassword) {
+    res.status(401).json({ message: "Password is incorrect" });
+    return;
+  }
+
+  const token = jwt.sign({ emailAdd: user.emailAdd }, process.env.SECRET_KEY, { expiresIn: '1h' });
+  res.cookie('authToken', token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: 'strict',
+  });
+  req.body.token = token;
+  next();
 };
 
-const verifyAToken = (req, res, next) => {
-    let { cookie } = req.headers;
-    let token = cookie && cookie.split('=')[1];
-    if (!token) {
-        res.status(401).json({ message: 'Token is missing' });
-        return;
+const verifyToken = (req, res, next) => {
+  const token = req.cookies.authToken || req.headers['x-access-token'];
+  if (!token) {
+    res.status(401).json({ message: "Token is missing" });
+    return;
+  }
+
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    if (err) {
+      res.status(401).json({ message: "Token is invalid" });
+      return;
     }
-    jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
-        if (err) {
-            res.status(401).json({ message: 'Token is invalid' });
-        } else {
-            req.body.emailAdd = decoded.emailAdd;
-            next();
-        }
-    });
+
+    req.body.emailAdd = decoded.emailAdd;
+    next();
+  });
 };
-export { checkUser, verifyAToken };
+
+export { authenticateUser, verifyToken };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { getUsers, insertUser, getUser, updateUser, deleteUser, loginUser } from '../controller/usersController.js'
-import { checkUser, verifyAToken } from '../middleware/authenticate.js' 
+import { authenticateUser, verifyToken } from '../middleware/authenticate.js' 
 import { fetchCarts, fetchuserCart, fetchadduserCart, fetchupdateUserCart, deleteCart, deleteItem } from '../model/cartDb.js'
 
 const userRouter = express.Router()
@@ -10,10 +10,10 @@ userRouter.get('/singleUser/:id', getUser)
 userRouter.post('/register', insertUser)
 userRouter.patch('/update/:id', updateUser)
 userRouter.delete('/delete/:id', deleteUser)
-userRouter.post('/login', loginUser)
+userRouter.post('/login', authenticateUser,loginUser)
 
-userRouter.get('/carts', fetchCarts)
-userRouter.get('/:id/cart', fetchuserCart)
+userRouter.get('/carts', verifyToken, fetchCarts)
+userRouter.get('/:id/cart', verifyToken, fetchuserCart)
 userRouter.post('/:id/cart', fetchadduserCart)
 userRouter.patch('/:id/cart/:prodID', fetchupdateUserCart)
 userRouter.delete('/:id/cart', deleteCart)
